c2cd80e9512215346c7ee0f0b1dd2eb7ac0ffe5a
Natalie Mastracci <mastracci.natalie@gmail.com>
2019-07-14 21:05:25 -0400

    test confirmed one can of baked beans

--- src/supermarket.ts
+++ src/supermarket.ts
@@ -1,5 +1,22 @@
-export function hello() {
-  return 'Hello World!';
+export enum AvailableGroceries {
+  bakedBeans,
+  bananas,
+  beer,
 }
 
-export default hello;
+export interface IGrocery {
+  lineItem: AvailableGroceries;
+  amount: number;
+}
+
+export function checkout(groceries: IGrocery[]): number {
+  return groceries.reduce((accum: number, curr: IGrocery): number => {
+    if (curr.lineItem === AvailableGroceries.bakedBeans) {
+      accum = +curr.amount * 0.2;
+      return accum;
+    }
+    return accum;
+  }, 0);
+}
+
+export default checkout;
--- src/tests/supermarket.spec.ts
+++ src/tests/supermarket.spec.ts
@@ -1,10 +1,31 @@
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
-import hello from './../supermarket';
+import { AvailableGroceries, checkout } from './../supermarket';
+
+// Prices and Sales
+// bakedBeans: 0.20 per can and 3 for 2,
+// bananas 1.20 per bag
+// beer 1.50/bottle and 3 for $4
 
 describe('Super Market', () => {
-  it('should run the hello function', () => {
-    const result = hello();
-    expect(result).to.equal('Hello World!');
+  it('should price one can of baked beans correctly', () => {
+    const result = checkout([
+      { lineItem: AvailableGroceries.bakedBeans, amount: 1 },
+    ]);
+    expect(result).to.equal(0.2);
+  });
+
+  xit('should price one bag of bananas correctly', () => {
+    const result = checkout([
+      { lineItem: AvailableGroceries.bananas, amount: 1 },
+    ]);
+    expect(result).to.equal(1.2);
+  });
+
+  xit('should price one bottle of beer correctly', () => {
+    const result = checkout([
+      { lineItem: AvailableGroceries.bananas, amount: 1 },
+    ]);
+    expect(result).to.equal(1.5);
   });
 });
