3b85629004892b815764386baaa8a7bbdb7cd8ff
Natalie Mastracci <mastracci.natalie@gmail.com>
2019-07-18 23:09:51 -0400

    adds tests

--- Ordinal-Numbers-typescript-mocha/src/tests/OrdinalNumbers.spec.ts
+++ Ordinal-Numbers-typescript-mocha/src/tests/OrdinalNumbers.spec.ts
@@ -8,4 +8,207 @@ describe('Making a Number Ordinal', () => {
 
     expect(result).to.equal('1st');
   });
+
+  it('turns 2 into 2nd', () => {
+    const digit = new Digit(2);
+    const result = digit.makeOrdinal();
+
+    expect(result).to.equal('2nd');
+  });
+
+  it('turns 3 into 3rd', () => {
+    const digit = new Digit(3);
+    const result = digit.makeOrdinal();
+
+    expect(result).to.equal('3rd');
+  });
+
+  it('turns 4 into 4th', () => {
+    const digit = new Digit(4);
+    const result = digit.makeOrdinal();
+
+    expect(result).to.equal('4th');
+  });
+
+  it('turns 5 into 5th', () => {
+    const digit = new Digit(5);
+    const result = digit.makeOrdinal();
+
+    expect(result).to.equal('5th');
+  });
+
+  it('turns 6 into 6th', () => {
+    const digit = new Digit(6);
+    const result = digit.makeOrdinal();
+
+    expect(result).to.equal('6th');
+  });
+
+  it('turns 7 into 7th', () => {
+    const digit = new Digit(7);
+    const result = digit.makeOrdinal();
+
+    expect(result).to.equal('7th');
+  });
+
+  it('turns 8 into 8th', () => {
+    const digit = new Digit(8);
+    const result = digit.makeOrdinal();
+
+    expect(result).to.equal('8th');
+  });
+
+  it('turns 9 into 9th', () => {
+    const digit = new Digit(9);
+    const result = digit.makeOrdinal();
+
+    expect(result).to.equal('9th');
+  });
+
+  it('turns 10 into 10th', () => {
+    const digit = new Digit(10);
+    const result = digit.makeOrdinal();
+
+    expect(result).to.equal('10th');
+  });
+
+  it('turns 11 into 11th', () => {
+    const digit = new Digit(11);
+    const result = digit.makeOrdinal();
+
+    expect(result).to.equal('11th');
+  });
+
+  it('turns 12 into 12th', () => {
+    const digit = new Digit(12);
+    const result = digit.makeOrdinal();
+
+    expect(result).to.equal('12th');
+  });
+
+  it('turns 13 into 13th', () => {
+    const digit = new Digit(13);
+    const result = digit.makeOrdinal();
+
+    expect(result).to.equal('13th');
+  });
+
+  it('turns 14 into 14th', () => {
+    const digit = new Digit(14);
+    const result = digit.makeOrdinal();
+
+    expect(result).to.equal('14th');
+  });
+
+  it('turns 15 into 15th', () => {
+    const digit = new Digit(15);
+    const result = digit.makeOrdinal();
+
+    expect(result).to.equal('15th');
+  });
+
+  it('turns 16 into 16th', () => {
+    const digit = new Digit(16);
+    const result = digit.makeOrdinal();
+
+    expect(result).to.equal('16th');
+  });
+
+  it('turns 17 into 17th', () => {
+    const digit = new Digit(17);
+    const result = digit.makeOrdinal();
+
+    expect(result).to.equal('17th');
+  });
+
+  it('turns 18 into 18th', () => {
+    const digit = new Digit(18);
+    const result = digit.makeOrdinal();
+
+    expect(result).to.equal('18th');
+  });
+
+  it('turns 19 into 19th', () => {
+    const digit = new Digit(19);
+    const result = digit.makeOrdinal();
+
+    expect(result).to.equal('19th');
+  });
+
+  it('turns 20 into 20th', () => {
+    const digit = new Digit(20);
+    const result = digit.makeOrdinal();
+
+    expect(result).to.equal('20th');
+  });
+
+  it('turns 21 into 21st', () => {
+    const digit = new Digit(21);
+    const result = digit.makeOrdinal();
+
+    expect(result).to.equal('21st');
+  });
+
+  it('turns 22 into 22nd', () => {
+    const digit = new Digit(22);
+    const result = digit.makeOrdinal();
+
+    expect(result).to.equal('22nd');
+  });
+
+  it('turns 23 into 23rd', () => {
+    const digit = new Digit(23);
+    const result = digit.makeOrdinal();
+
+    expect(result).to.equal('23rd');
+  });
+
+  it('turns 24 into 24th', () => {
+    const digit = new Digit(24);
+    const result = digit.makeOrdinal();
+
+    expect(result).to.equal('24th');
+  });
+
+  it('turns 25 into 25th', () => {
+    const digit = new Digit(25);
+    const result = digit.makeOrdinal();
+
+    expect(result).to.equal('25th');
+  });
+
+  it('turns 26 into 26th', () => {
+    const digit = new Digit(26);
+    const result = digit.makeOrdinal();
+
+    expect(result).to.equal('26th');
+  });
+
+  it('turns 27 into 27th', () => {
+    const digit = new Digit(27);
+    const result = digit.makeOrdinal();
+
+    expect(result).to.equal('27th');
+  });
+
+  it('turns 28 into 28th', () => {
+    const digit = new Digit(28);
+    const result = digit.makeOrdinal();
+
+    expect(result).to.equal('28th');
+  });
+
+  it('turns 29 into 29th', () => {
+    const digit = new Digit(29);
+    const result = digit.makeOrdinal();
+
+    expect(result).to.equal('29th');
+  });
+
+  it('turns 30 into 30th', () => {
+    const digit = new Digit(30);
+    const result = digit.makeOrdinal();
+
+    expect(result).to.equal('30th');
+  });
 });
