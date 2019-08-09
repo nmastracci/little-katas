ea1a04592996f47bc28fd77a7bc380b5e675ff38
Natalie <mastracci.natalie@gmail.com>
2019-08-09 08:50:35 -0400

    adds all known tests to the system for refactoring

--- Guilded-Rose-typescript-mocha/src/tests/GuildedRose.spec.ts
+++ Guilded-Rose-typescript-mocha/src/tests/GuildedRose.spec.ts
@@ -5,7 +5,6 @@ import { Item } from '../Item';
 
 describe('Guilded Rose Items', () => {
   it('should degrade sellin and quality value by one each day for a normal item', () => {
-    // const item = new Item('Normal', 5, 5 )
     const shop = new Shop([{name:'Normal', sellIn:5, quality:5 }]);
     const result = shop.updateQuality()
 
@@ -14,20 +13,69 @@ describe('Guilded Rose Items', () => {
     expect(result[0].quality).to.equal(4);
   });
 
+  it('should degrade Quality twice as fast past the sell by date for a normal item', () => {
+    const shop = new Shop([{name: 'Normal', sellIn: 0, quality:5}]);
+    const result = shop.updateQuality();
+
+    expect(result[0].name).to.equal('Normal');
+    expect(result[0].sellIn).to.equal(-1);
+    expect(result[0].quality).to.equal(3);
+  });
+
   it('should not have a quality greater than 50', () => {
-    const shop50 = new Shop([{name: 'Aged Brie', sellIn: 1, quality: 50}]);
-    const oneDayOn50 = shop50.updateQuality();
+    const shop = new Shop([{name: 'Aged Brie', sellIn: 3, quality: 49}]);
+    const result = shop.updateQuality();
 
-    expect(oneDayOn50[0].quality).to.equal(50);
+    expect(result[0].name).to.equal('Aged Brie');
+    expect(result[0].quality).to.equal(50);
+    expect(result[0].sellIn).to.equal(2);
 
+
+    const secondResult = shop.updateQuality();
+
+    expect(secondResult[0].name).to.equal('Aged Brie');
+    expect(secondResult[0].quality).to.equal(50);
+    expect(secondResult[0].sellIn).to.equal(1);
+
+    const thirdResult = shop.updateQuality();
+
+    expect(thirdResult[0].name).to.equal('Aged Brie');
+    expect(thirdResult[0].quality).to.equal(50);
+    expect(thirdResult[0].sellIn).to.equal(0);
   });
 
   it('should not have a quality greater than 50 for backstage passes', () => {
-    const shop50 = new Shop([{name: 'Backstage passes to a TAFKAL80ETC concert', sellIn: 50, quality: 50}]);
-    const oneDayOn50 = shop50.updateQuality();
+    const shop = new Shop([{name: 'Backstage passes to a TAFKAL80ETC concert', sellIn: 50, quality: 49}]);
+    const result = shop.updateQuality();
 
-    expect(oneDayOn50[0].quality).to.equal(50);
+    expect(result[0].name).to.equal('Backstage passes to a TAFKAL80ETC concert');
+    expect(result[0].quality).to.equal(50);
+    expect(result[0].sellIn).to.equal(49);
 
+    const secondResult = shop.updateQuality();
+
+    expect(secondResult[0].name).to.equal('Backstage passes to a TAFKAL80ETC concert');
+    expect(secondResult[0].quality).to.equal(50);
+    expect(secondResult[0].sellIn).to.equal(48);
+
+    const thirdResult = shop.updateQuality();
+
+    expect(thirdResult[0].name).to.equal('Backstage passes to a TAFKAL80ETC concert');
+    expect(thirdResult[0].quality).to.equal(50);
+    expect(thirdResult[0].sellIn).to.equal(47);
+  });
+
+  it('should increase Aged Brie and Backstage Passes quality over time', () => {
+    const shop = new Shop([{name: 'Aged Brie', sellIn: 10, quality: 20}, {name: 'Backstage passes to a TAFKAL80ETC concert', sellIn: 20, quality: 20}]);
+    const result = shop.updateQuality();
+
+    const AgedBrie = result.filter(x => x.name === 'Aged Brie');
+    const BackstagePasses = result.filter(x => x.name === 'Backstage passes to a TAFKAL80ETC concert');
+
+    expect(AgedBrie[0].quality).to.equal(21);
+    expect(AgedBrie[0].sellIn).to.equal(9);
+    expect(BackstagePasses[0].quality).to.equal(21);
+    expect(BackstagePasses[0].sellIn).to.equal(19);
   });
 
   it('backstage pass: 10 days or less Quality increases by 2', () => {
@@ -36,28 +84,31 @@ describe('Guilded Rose Items', () => {
     expect(result[0].quality).to.equal(22);
   });
 
-  it('backstage pass: 10 days or more Quality increases by 1', () => {
-    let shop = new Shop([{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 12, quality: 20}]);
-    let result = shop.updateQuality()
-    expect(result[0].quality).to.equal(21);
-
-    shop = new Shop([{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 11, quality: 20}]);
-    result = shop.updateQuality()
-    expect(result[0].quality).to.equal(21);
-
-  });
-
   it('backstage pass: 5 days or less Quality increases by 3', () => {
     const shop = new Shop([{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 5, quality: 20}]);
     const result = shop.updateQuality()
 
     expect(result[0].quality).to.equal(23);
-
-    // shop[0].sellIn = 5;
-
-
-
   });
+
+  it('backstage pass: quality is 0 after concert', () => {
+    const shop = new Shop([{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 0, quality: 20}]);
+    const result = shop.updateQuality()
+
+    expect(result[0].sellIn).to.equal(-1);
+    expect(result[0].quality).to.equal(0);
+  });
+
+  // TODO: implement solution for conjured items after refactoring
+  xit('degrades the quality of Conjured Items twice as fast', () => {
+    const shop = new Shop([{name: "Conjured", sellIn: 5, quality: 20}]);
+    const result = shop.updateQuality()
+
+    expect(result[0].sellIn).to.equal(4);
+    expect(result[0].quality).to.equal(18);
+  });
+
+
 
 });
 
