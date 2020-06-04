111a7b9723c2348eea79a35d1613d247d310fad1
nmastracci <mastracci.natalie@gmail.com>
2020-06-04 16:27:50 -0400

    first session

--- Guilded-Rose-typescript-mocha/src/Shop.ts
+++ Guilded-Rose-typescript-mocha/src/Shop.ts
@@ -1,5 +1,9 @@
 import { IItem } from "./Item";
-
+const items ={
+  brie: "Aged Brie",
+  backstagePasses: "Backstage passes to a TAFKAL80ETC concert",
+  sulfuras: "Sulfuras, Hand of Ragnaros"
+}
 export default class Shop {
   items: IItem[]
   constructor(items: IItem[]){
@@ -7,16 +11,16 @@ export default class Shop {
   }
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (this.items[i].name !== items.brie && this.items[i].name !== items.backstagePasses) {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+          if (this.items[i].name !== items.sulfuras) {
             this.items[i].quality = this.items[i].quality - 1;
           }
         }
       } else {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+          if (this.items[i].name === items.backstagePasses) {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1;
@@ -30,14 +34,14 @@ export default class Shop {
           }
         }
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+      if (this.items[i].name !== items.sulfuras) {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (this.items[i].name !== items.brie) {
+          if (this.items[i].name !== items.backstagePasses) {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+              if (this.items[i].name !== items.sulfuras) {
                 this.items[i].quality = this.items[i].quality - 1;
               }
             }
--- Guilded-Rose-typescript-mocha/src/tests/GuildedRose.spec.ts
+++ Guilded-Rose-typescript-mocha/src/tests/GuildedRose.spec.ts
@@ -7,11 +7,89 @@ describe('Guilded Rose Items', () => {
   it('correctly addresses a normal item', () => {
     // const item = new Item('Normal', 5, 5 )
     const shop = new Shop([{name:'Normal', sellIn:5, quality:5 }]);
-    const result = shop.updateQuality()
+    const result = shop.updateQuality();
 
     expect(result[0].name).to.equal('Normal');
     expect(result[0].sellIn).to.equal(4);
     expect(result[0].quality).to.equal(4);
+  });
+
+  it('correctly degrades a normal item quality when past sellIn date', () => {
+    // const item = new Item('Normal', 5, 5 )
+    const shop = new Shop([{name:'Normal', sellIn:0, quality:5 }]);
+    const result = shop.updateQuality();
+
+    expect(result[0].name).to.equal('Normal');
+    expect(result[0].sellIn).to.equal(-1);
+    expect(result[0].quality).to.equal(3);
+  });
+
+  it('can not have quality greater than 50', () => {
+    // const item = new Item('Normal', 5, 5 )
+    const shop = new Shop([{name:'Normal', sellIn:5, quality:51 }]);
+    const result = shop.updateQuality();
+
+    expect(result[0].name).to.equal('Normal');
+    expect(result[0].sellIn).to.equal(4);
+    expect(result[0].quality).to.equal(50);
+  });
+
+  it('increases in quality over time if it is Aged Brie', () => {
+    const shop = new Shop([{name:'Aged Brie', sellIn:5, quality:40 }]);
+    const result = shop.updateQuality();
+
+    expect(result[0].name).to.equal('Aged Brie');
+    expect(result[0].sellIn).to.equal(4);
+    expect(result[0].quality).to.be.greaterThan(40);
+  });
+
+  it('Sulfuras, has no SellIn date and never decreases in Quality', () => {
+    const shop = new Shop([{ name: 'Sulfuras, Hand of Ragnaros', sellIn: 0, quality: 40 }]);
+    const result = shop.updateQuality();
+
+    expect(result[0].name).to.equal('Sulfuras, Hand of Ragnaros');
+    expect(result[0].sellIn).to.equal(0);
+    expect(result[0].quality).to.equal(40);
+  });
+
+  it('generally increases quality as sellin value approaches for Backstage passes', () => {
+    const shop = new Shop([{ name: 'Backstage passes to a TAFKAL80ETC concert', sellIn: 15, quality: 40 }]);
+    const result = shop.updateQuality();
+
+    expect(result[0].name).to.equal('Backstage passes to a TAFKAL80ETC concert');
+    expect(result[0].sellIn).to.equal(14);
+    expect(result[0].quality).to.equal(41);
+  });
+
+  it('increases quality by two if sellIn date is 10 or lower', () => {
+    const shop = new Shop([{ name: 'Backstage passes to a TAFKAL80ETC concert', sellIn: 11, quality: 40 }]);
+    let result = shop.updateQuality();
+
+    expect(result[0].name).to.equal('Backstage passes to a TAFKAL80ETC concert');
+    expect(result[0].sellIn).to.equal(10);
+    expect(result[0].quality).to.equal(41);
+    shop.updateQuality();
+    expect(result[0].quality).to.equal(43);
+    expect(result[0].sellIn).to.equal(9);
+    
+  });
+
+  it('increases quality by three if sellIn date is 5 or lower', () => {
+    const shop = new Shop([{ name: 'Backstage passes to a TAFKAL80ETC concert', sellIn: 5, quality: 40 }]);
+    const result = shop.updateQuality();
+
+    expect(result[0].name).to.equal('Backstage passes to a TAFKAL80ETC concert');
+    expect(result[0].sellIn).to.equal(4);
+    expect(result[0].quality).to.equal(43);
+  });
+
+  it('sets quality to zero if sellIn is lower than 0', () => {
+    const shop = new Shop([{ name: 'Backstage passes to a TAFKAL80ETC concert', sellIn: 0, quality: 40 }]);
+    const result = shop.updateQuality();
+
+    expect(result[0].name).to.equal('Backstage passes to a TAFKAL80ETC concert');
+    expect(result[0].sellIn).to.equal(-1);
+    expect(result[0].quality).to.equal(0);
   });
 });
 
